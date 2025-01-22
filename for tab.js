43600de8a07221e03f420094df91c40ed7eb7b75
RabiaKhalidButt <Rabia_khalid9@hotmail.com>
2025-01-23 03:05:49 +0500

    Add files via upload

---
+++ for tab.js
@@ -0,0 +1,6 @@
+tab_value;()=> {
+    let Tab_name=document.getElementById("tb1").value;
+    for(let k=1;k<=10;k++)
+document.write( Tab_name+ "x"+k+"="+Tab_name*k);
+ console.log( Tab_name+ "x"+k+"="+Tab_name*k) ; 
+}
